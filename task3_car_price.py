b9e1bb4a9a70a190cc3e477ae740ffbbbc6668c3
divyanA615-web <divyandon615@gmail.com>
2026-06-03 14:52:08 +0530

    Add data loading and initial analysis for car price prediction

--- task3_car_price.py
+++ task3_car_price.py
@@ -15,3 +15,10 @@ from sklearn.preprocessing import LabelEncoder # type: ignore
 from sklearn.linear_model import LinearRegression # type: ignore
 from sklearn.ensemble import RandomForestRegressor # type: ignore
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error # type: ignore
+
+# ── 1. LOAD ──────────────────────────────────────────────────
+df = pd.read_csv("D:\data science related\CodeAlpha_Internship\CodeAlpha_Unemployment-Car-Price-Prediction-with-Machine-Learning\car_data.csv") # type: ignore
+print(f"Shape: {df.shape}")
+print(df.head())
+print(f"\nMissing:\n{df.isnull().sum()}")
+print(f"\nDescriptive Stats:\n{df.describe().round(2)}")
